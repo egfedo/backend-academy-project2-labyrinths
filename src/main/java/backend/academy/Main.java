16ab3237c4d733fecf66ccb058b0dd28832f838c
egfedo <fegor04@gmail.com>
2024-10-27 18:46:36 +0300

    feat: update main to start program

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,15 @@
 package backend.academy;
 
+import backend.academy.egfedo.manager.ProgramManager;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
+
     public static void main(String[] args) {
+
+        var program = new ProgramManager();
+        program.run();
+
     }
 }
