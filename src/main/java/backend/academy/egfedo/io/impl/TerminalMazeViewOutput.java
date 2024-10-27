8181271ba74d09c9e2979f7447bc43ccc63a485d
egfedo <fegor04@gmail.com>
2024-10-27 18:40:50 +0300

    feat: add TerminalMazeViewOutput

---
+++ src/main/java/backend/academy/egfedo/io/impl/TerminalMazeViewOutput.java
@@ -0,0 +1,140 @@
+package backend.academy.egfedo.io.impl;
+
+import backend.academy.egfedo.data.Direction;
+import backend.academy.egfedo.data.Vector;
+import backend.academy.egfedo.io.MazeTile;
+import backend.academy.egfedo.io.MazeViewOutput;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import org.jline.terminal.Terminal;
+import org.jline.utils.InfoCmp;
+
+public class TerminalMazeViewOutput implements MazeViewOutput  {
+
+    private final Terminal terminal;
+    private final PrintWriter writer;
+    private final Vector dimensions;
+
+    private final static String HEADING = "[ MAZE VIEW ]";
+    private final Map<MazeTile, String> tileToString = Map.of(
+        MazeTile.FLOOR, "  ",
+        MazeTile.WALL, "⬜",
+        MazeTile.EMPTY, "  ",
+        MazeTile.PATH, "\uD83D\uDFE1",
+        MazeTile.START, "\uD83D\uDD34",
+        MazeTile.END, "\uD83D\uDFE2",
+        MazeTile.MUD, "\uD83D\uDFEB",
+        MazeTile.MUD_PATH, "\uD83D\uDFE4",
+        MazeTile.ICE, "\uD83D\uDFE6",
+        MazeTile.ICE_PATH, "\uD83D\uDD35"
+    );
+
+    public TerminalMazeViewOutput(Terminal terminal, Vector dimensions) {
+        this.terminal = Objects.requireNonNull(terminal);
+        this.writer = terminal.writer();
+        this.dimensions = Objects.requireNonNull(dimensions);
+        terminal.puts(InfoCmp.Capability.clear_screen);
+        terminal.puts(InfoCmp.Capability.cursor_invisible);
+    }
+
+    @SuppressFBWarnings(value = "IM_BAD_CHECK_FOR_ODD", justification = "Counter cannot be negative")
+    // Display settings are not editable
+    @SuppressWarnings({"MagicNumber", "MultipleStringLiterals"})
+    @Override
+    public void displayMaze(List<List<MazeTile>> fragment, Vector offset, Map<Direction, Boolean> fogFlags) {
+        terminal.puts(InfoCmp.Capability.cursor_address, 0, 0);
+        writer.println(" ".repeat(terminal.getWidth() / 2 - Math.ceilDiv(HEADING.length(), 2)) + HEADING);
+        writer.print("        ");
+
+        for (int j = offset.x(); j < offset.x() + dimensions.x(); j++) {
+            if (j < 10) {
+                writer.print(' ');
+            }
+            writer.print(j);
+            writer.print("  ");
+        }
+        writer.println();
+        if (fogFlags.get(Direction.UP)) {
+            writer.print("      ");
+            writer.print("^".repeat(dimensions.x() * 4 + 2));
+        } else {
+            writer.print("      ");
+            writer.print(" ".repeat(dimensions.x() * 4 + 2));
+        }
+        writer.println();
+
+        int y = offset.y();
+        int counter = 0;
+        for (var row : fragment) {
+            if (counter % 2 == 1) {
+                writer.print(" ");
+                if (y < 10) {
+                    writer.print(' ');
+                }
+                writer.print(y);
+                writer.print(" ");
+            } else {
+                writer.print("    ");
+            }
+
+            if (fogFlags.get(Direction.LEFT)) {
+                writer.print("< ");
+            } else {
+                writer.print("  ");
+            }
+
+            for (var elem : row) {
+                writer.print(tileToString.get(elem));
+            }
+
+            if (fogFlags.get(Direction.RIGHT)) {
+                writer.print(" >");
+            } else {
+                writer.print("  ");
+            }
+
+            if (counter % 2 == 1) {
+                writer.print(" ");
+                if (y < 10) {
+                    writer.print(' ');
+                }
+                writer.print(y);
+                writer.print(" ");
+                y++;
+            }
+
+            counter++;
+
+            writer.println();
+        }
+        if (fogFlags.get(Direction.DOWN)) {
+            writer.print("      ");
+            writer.print("V".repeat(dimensions.x() * 4 + 2));
+        } else {
+            writer.print("      ");
+            writer.print(" ".repeat(dimensions.x() * 4 + 2));
+        }
+
+        writer.println();
+        writer.print("        ");
+        for (int j = offset.x(); j < offset.x() + dimensions.x(); j++) {
+            if (j < 10) {
+                writer.print(' ');
+            }
+            writer.print(j);
+            writer.print("  ");
+        }
+        writer.println();
+        writer.println();
+        writer.print(" [W, A, S, D] - Move  [esc] - Exit");
+        writer.flush();
+    }
+
+    @Override
+    public Vector getDimensions() {
+        return dimensions;
+    }
+}
