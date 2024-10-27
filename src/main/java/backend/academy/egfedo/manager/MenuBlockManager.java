10299bfb421bc6abc2488b3b49cf1796cf73459d
egfedo <fegor04@gmail.com>
2024-10-27 18:44:21 +0300

    feat: add MenuBlockManager interface

---
+++ src/main/java/backend/academy/egfedo/manager/MenuBlockManager.java
@@ -0,0 +1,9 @@
+package backend.academy.egfedo.manager;
+
+import backend.academy.egfedo.data.ProgramOptions;
+
+public interface MenuBlockManager {
+
+    void proceed(ProgramOptions.ProgramOptionsBuilder options);
+
+}
