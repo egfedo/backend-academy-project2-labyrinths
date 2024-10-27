0feddebcfc19926c01146569917affcebca5c7d8
egfedo <fegor04@gmail.com>
2024-10-27 18:32:42 +0300

    feat: add RandomProvider util class

---
+++ src/main/java/backend/academy/egfedo/util/RandomProvider.java
@@ -0,0 +1,20 @@
+package backend.academy.egfedo.util;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.security.SecureRandom;
+import java.util.Random;
+import lombok.experimental.UtilityClass;
+
+@SuppressFBWarnings("MS_EXPOSE_REP")
+@UtilityClass
+public class RandomProvider {
+
+    static private Random instance = null;
+
+    public static Random getInstance() {
+        if (instance == null) {
+            instance = new SecureRandom();
+        }
+        return instance;
+    }
+}
